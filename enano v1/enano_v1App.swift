62a8bc64d55bcabaf10cdff2a066a0b84ea14ca3
jnj1115 <awang502@berkeley.edu>
2024-06-17 16:56:09 -0700

    Initial Commit

---
+++ enano v1/enano_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  enano_v1App.swift
+//  enano v1
+//
+//  Created by Andrew Wang on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct enano_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
